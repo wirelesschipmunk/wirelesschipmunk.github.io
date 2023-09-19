83a4e6b39e03199b4f66126f767dc21b6e2504c5
wirelesschipmunk <thomasisaac284@gmail.com>
2023-09-19 10:42:42 -0600

    Added projects bit

---
+++ src/lib/TopBar.svelte
@@ -0,0 +1,58 @@
+<div>
+	<img src="src/lib/images/slugcat-pfp.png" alt="slugcat-pfp" />
+
+	<h1>wirelesschipmunk</h1>
+
+	<button
+		style="margin-left: auto;"
+		on:click={() => (window.location = "https://github.com/wirelesschipmunk/")}>github!</button
+	>
+	<button>about!</button>
+	<button>&#9775;</button>
+	<button>&#9776;</button>
+</div>
+
+<style>
+	div {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
+		gap: 0.5em;
+		background-color: rgb(28, 28, 28);
+		top: 0;
+		width: 100%;
+		position: fixed;
+	}
+
+	img {
+		border-radius: 2em;
+		width: 55px;
+		box-shadow: 2px 2px 0px rebeccapurple;
+		border: solid;
+		border-color: gray;
+		border-width: 1.85px;
+		margin-left: 0.3em;
+	}
+
+	button {
+		font: inherit;
+		color: whitesmoke;
+		background-color: rgb(55, 55, 55);
+		border: solid;
+		border-color: gray;
+		border-width: 1.85px;
+		border-radius: 10px;
+		box-shadow: 2px 2px 0px rebeccapurple;
+		padding: 0.6em 1.5em;
+		margin-right: 0.3em;
+		cursor: pointer;
+		transition: all 0.15s;
+	}
+
+	button:active {
+		background-color: rebeccapurple;
+		box-shadow: 1px 1px 0px rebeccapurple;
+		opacity: 0.8;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,78 @@
+<script>
+	import TopBar from "../lib/TopBar.svelte";
+
+	const projects = [
+		{
+			name: "Dumplings Yum!",
+			repoLink: "https://github.com/wirelesschipmunk/dumplings-yum",
+			siteLink: "https://wirelesschipmunk.com/dumplings-yum/",
+			description:
+				"A nice looking restuarant website for chinese food. I also have a svelte version."
+		},
+
+		{
+			name: "Epic Trivia",
+			repoLink: "https://github.com/wirelesschipmunk/epic-trivia",
+			siteLink: "https://wirelesschipmunk.com/epic-trivia/",
+			description:
+				"Terrible triva ok design. I got the colors from the vscode theme Campfire Syntax made by Nicholas Morrison."
+		},
+
+		{
+			name: "HackerType (so original name)",
+			repoLink: "https://github.com/wirelesschipmunk/hacker-type",
+			siteLink: "https://wirelesschipmunk.com/hacker-type/",
+			description: "Small clone of HackerTyper.com. Missing some features."
+		},
+
+		{
+			name: "Rock Paper Scissors",
+			repoLink: "https://github.com/wirelesschipmunk/hacker-type",
+			siteLink: "https://wirelesschipmunk.com/hacker-type/",
+			description: "My first project! Arguably the best. I'm planning to add a computer version."
+		}
+	];
+</script>
+
+<TopBar />
+
+<h1 id="top">projects:</h1>
+
+{#each projects as project}
+	<div>
+		<h1>{project.name}</h1>
+		<h2>repo: <a href={project.repoLink}>{project.repoLink}</a></h2>
+		<h2>site: <a href={project.siteLink}>{project.siteLink}</a></h2>
+		<h4>{project.description}</h4>
+	</div>
+{/each}
+
+<style>
+	:global(body) {
+		font-family: "Alpino", sans-serif;
+		color: whitesmoke;
+		background-color: rgb(28, 28, 28);
+		padding: 0;
+		margin: 0;
+	}
+
+	h1 {
+		margin: 0.8em;
+	}
+
+	#top {
+		margin-top: 4em;
+	}
+
+	div {
+		margin-top: 4em;
+		background-color: rgb(48, 48, 48);
+		border-radius: 1em;
+		padding: 0.6em;
+		margin: 1em;
+	}
+
+	a {
+		color: rebeccapurple;
+	}
+</style>
